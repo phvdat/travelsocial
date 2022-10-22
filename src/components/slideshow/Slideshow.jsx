3d539831f4cd72f62af5499b24adddc56a8b4ab8
phvdat <phamvandat.hcmut@gmail.com>
2022-10-22 17:45:17 +0700

    update carousel with antd library

--- src/components/slideshow/Slideshow.jsx
+++ src/components/slideshow/Slideshow.jsx
@@ -1,49 +1,23 @@
 
+import { Carousel } from "antd";
 import React, { useEffect, useState } from "react";
 import "./slideshow.scss"
 export default function Slideshow() {
-	const [show, setShow] = useState(1)
-	const handleSlideShow = (val) => {
-		if (show == 1 && val == -1) {
-			setShow(3)
-		}
-		else if (show == 3 && val == 1) {
-			setShow(1)
-		}
-		else {
-			setShow(val + show)
-		}
-	}
 	return (
-		<div className="slideshow-container">
-			{
-				show == 1 ?
-					<div className="mySlides fade">
-						<img src="img/slideshow-2.jpg" className="imgSlideshow" />
-						<div className="text">Caption Text</div>
-					</div>
-					: <div style={{ display: "none" }}></div>
-			}
-			{
-				show == 2 ?
-					<div className="mySlides fade">
-						<img src="img/slideshow-1.jpg" className="imgSlideshow" />
-						<div className="text">Caption Text</div>
-					</div>
-					: <div style={{ display: "none" }}></div>
-			}
-			{
-				show == 3 ?
-					<div className="mySlides fade">
-						<img src="img/slideshow-3.jpg" className="imgSlideshow" />
-						<div className="text">Caption Text</div>
-					</div>
-					: <div style={{ display: "none" }}></div>
-			}
-
-			<a className="prev" onClick={() => { handleSlideShow(-1) }}>&#10094;</a>
-			<a className="next" onClick={() => { handleSlideShow(1) }}>&#10095;</a>
-
-		</div>
+		<Carousel>
+			<div className='carousel-container'>
+				<div className="text-carousel">ĐI VÀ KHÁM PHÁ THEO CÁCH CỦA BẠN</div>
+				<img alt="carousel" src="img/slideshow-1.jpg" className="imgSlideshow" />
+			</div>
+			<div>
+				<img alt="carousel" src="img/slideshow-2.jpg" className="imgSlideshow" />
+			</div>
+			<div>
+				<img alt="carousel" src="img/slideshow-3.jpg" className="imgSlideshow" />
+			</div>
+			<div>
+				<img alt="carousel" src="img/slideshow-2.jpg" className="imgSlideshow" />
+			</div>
+		</Carousel>
 	)
 }
