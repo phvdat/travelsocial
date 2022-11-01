4dcae081c51aa8db8fe7db8ffe29273f6434d228
phvdat <phamvandat.hcmut@gmail.com>
2022-11-02 01:27:42 +0700

    handle click to change tab in profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,12 +1,16 @@
 import { Col, message, Row } from "antd";
 import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import postApi from "../../api/postApi";
 import Post from "../../components/post/Post";
 import Share from "../../components/share/Share";
 import Topbar from "../../components/topbar/Topbar";
+import ProfileAbout from "./components/ProfileAbout";
+import ProfileFollow from "./components/ProfileFollow";
 import "./profile.scss";
 
 export default function Profile() {
+	let { tab } = useParams()
 	const [listPost, setListPost] = useState([])
 	useEffect(() => {
 		const getAllPost = async () => {
@@ -34,48 +38,68 @@ export default function Profile() {
 		<div className="container">
 			<Topbar />
 			<div className="profile">
-				<div className="topProfile">
-					<Row justify='center'>
-						<Col span={16} className="profile-image">
-							<img src="img/dulichvietnam.jpg" alt="cover Img" className="coverImg" />
-							<div className="containAvtImg">
-								<div className="avataProfile">
-									<img src="img/myavt.jpg" alt="avt Img" className="avataProfileImg" />
+				<Row justify='center'>
+					<Col span={16} className="profile-image">
+						<img src="img/dulichvietnam.jpg" alt="cover Img" className="coverImg" />
+						<div className="containAvtImg">
+							<div className="avataProfile">
+								<img src="img/myavt.jpg" alt="avt Img" className="avataProfileImg" />
+							</div>
+							<p className="text-name-user-top-profile">Phạm Văn Đạt</p>
+							<p className="text-top-profile">Cấp thành viên:<span style={{ fontWeight: 500 }}>VIP</span></p>
+							<div>
+								<span style={{ marginRight: 50 }}>Bài đã đăng: <span style={{ fontWeight: 500 }}>23</span></span>
+								<span>Lượt theo dõi: <span style={{ fontWeight: 500 }}>182</span></span>
+							</div>
+						</div>
+						<hr className="rightbarHr" />
+						<div className="controlProfile">
+							<div className="sub-controlProfile">
+								<div className={tab === 'newfeed' ? "tab-profile-active" : "tab-profile"}>
+									<Link to='/profile/newfeed'>
+										Bài viết
+									</Link>
 								</div>
-								<p className="text-name-user-top-profile">Phạm Văn Đạt</p>
-								<p className="text-top-profile">Cấp thành viên:<span style={{ fontWeight: 500 }}>VIP</span></p>
-								<div>
-									<span style={{ marginRight: 50 }}>Bài đã đăng: <span style={{ fontWeight: 500 }}>23</span></span>
-									<span>Lượt theo dõi: <span style={{ fontWeight: 500 }}>182</span></span>
+								<div className={tab === 'about' ? "tab-profile-active" : "tab-profile"}>
+									<Link to='/profile/about'>
+										Giới thiệu
+									</Link>
+								</div>
+								<div className={tab === 'follow' ? "tab-profile-active" : "tab-profile"}>
+									<Link to='/profile/follow'>
+										Đang theo dõi
+									</Link>
 								</div>
 							</div>
-							<hr className="rightbarHr" />
-							<div className="controlProfile">
-								<div className="sub-controlProfile">
-									<div className={true ? "itemActive" : "itemcontrol"}>Bài viết</div>
-									<div className="itemcontrol">Giới thiệu</div>
-									<div className="itemcontrol">Bạn bè</div>
-								</div>
-								<button className="btn-follow">{true ? "Theo dõi" : "Đăng theo dõi"}</button>
-							</div>
-						</Col>
-					</Row>
-				</div>
+							<button className="btn-follow">{true ? "Theo dõi" : "Đăng theo dõi"}</button>
+						</div>
+					</Col>
+				</Row>
 				<div className="downProfile">
 					<Row justify="center">
-						{/* <Col span={2}></Col> */}
-						<Col span={12}>
-							<Share />
-							{listPost.map((ele, idx) => {
-								return <Post data={ele} key={idx} />
-							})
+						<Col span={16} >
+							{
+								tab === 'newfeed' &&
+								<>
+									<Share />
+									{listPost.map((ele, idx) => {
+										return <Post data={ele} key={idx} />
+									})
+									}
+								</>
+							}
+							{
+								tab === 'about' &&
+								<ProfileAbout />
+							}
+							{
+								tab === 'follow' &&
+								<ProfileFollow />
 							}
 						</Col>
-						{/* <Col span={2}></Col> */}
-
 					</Row>
 				</div>
 			</div>
-		</div>
+		</div >
 	)
 }
