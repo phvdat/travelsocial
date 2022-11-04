3023be17b3ebbd1f5443d3ad1b23aa55ae7fc2f2
PhamDat2000 <68559352+PhamDat2000@users.noreply.github.com>
2022-11-04 20:06:04 +0700

    test 7

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import postApi from '../../api/postApi';
 import reactPostApi from '../../api/reactPostApi';
 export default function Post(props) {
-	// const data = props.data
+	const data = props.data
 	// const navigate = useNavigate()
 	// const [like, setLike] = useState(false)
 	// const [showComment, setShowComment] = useState(false)
