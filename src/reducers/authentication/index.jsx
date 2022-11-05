e63e2a6aed9f747af3780e3f10bd5619092f1bc2
phvdat <phamvandat.hcmut@gmail.com>
2022-11-05 16:38:39 +0700

    config store save authentication info

--- src/reducers/authentication/index.jsx
+++ src/reducers/authentication/index.jsx
@@ -1,18 +1,18 @@
 import { LOGIN_SUCCESS, LOGOUT_SUCCESS, SET_CURRENT_USER } from "./actionTypes"
 const initialState = {
 	isLoggedIn: localStorage.getItem('isLogin') ? JSON.parse(localStorage.getItem('isLogin')) : false,
-	userInfo: localStorage.getItem('user_info') ? JSON.parse(localStorage.getItem('user_info')) : {},
+	currentUser: localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : {},
 };
 export const authentication = (state = initialState, action) => {
 	switch (action.type) {
 		case LOGIN_SUCCESS:
-			const newState = { isLoggedIn: true }
+			const newState = { ...state, isLoggedIn: true }
 			return newState
 		case LOGOUT_SUCCESS:
-			return { isLoggedIn: false , userInfo: {}}
+			return { isLoggedIn: false, currentUser: {} }
 		case SET_CURRENT_USER:
-			return action.payload
-		
+			return { ...state, currentUser: action.payload }
+
 		default:
 			return state
 	}
