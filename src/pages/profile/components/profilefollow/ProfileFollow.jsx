96a6a84d7600569b1717db0bdf80d658811c0ac9
phvdat <phamvandat.hcmut@gmail.com>
2022-11-13 12:31:17 +0700

    implement call data for this page

--- src/pages/profile/components/profilefollow/ProfileFollow.jsx
+++ src/pages/profile/components/profilefollow/ProfileFollow.jsx
@@ -1,36 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Dropdown, Menu, Row } from "antd";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import './profileFollow.scss'
+import followApi from 'api/followApi';
+import authApi from 'api/authApi';
+import { Link } from 'react-router-dom';
 export default function ProfileFollow(props) {
 	const { typetab } = props
-	const data = [
-		{
-			imgUrl:
-				"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png",
-			name: "Nguyễn Văn A"
-		},
-		{
-			imgUrl:
-				"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png",
-			name: "Nguyễn Văn A"
-		},
-		{
-			imgUrl:
-				"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png",
-			name: "Nguyễn Văn A"
-		},
-		{
-			imgUrl:
-				"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png",
-			name: "Nguyễn Văn A"
-		},
-		{
-			imgUrl:
-				"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png",
-			name: "Nguyễn Văn A"
+	const [usersInfo, setUsersInfo] = useState([])
+	useEffect(() => {
+		window.scrollTo(0, 0);
+		const params = {
+			userId: usersInfo._id,
+			page: 1,
+			size: 10
 		}
-	];
+		const getFollower = async () => {
+			try {
+				const response = await followApi.getFollower(params)
+				if (response.status_code === 9999) {
+					return response.payload.map((ele) => ele.userIdTarget)
+				}
+				if (response.status_code === -9999) {
+					console.log(response.message)
+				}
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		getFollower().then((res, req) =>
+			console.log(res)
+		)
+		const getFollowUser = async () => {
+			try {
+				const response = await followApi.getFollowUser(params)
+				if (response.status_code === 9999) {
+					const data = response.payload.map((ele) => ele.userId)
+					return data
+				}
+				if (response.status_code === -9999) {
+					console.log(response.message)
+				}
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		const getUsersInfo = async (id) => {
+			try {
+				const param = { userId: id }
+				const response = await authApi.getUserInfoApi(param)
+				if (response.status_code === 9999) {
+					console.log('aaaaaaaaaa', response.payload)
+					setUsersInfo((state) => [...state, response.payload])
+				}
+				if (response.status_code === -9999) {
+					console.log(response.message)
+				}
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		if (typetab === 'follower') {
+			getFollower().then((res, req) => {
+				res.map((id) => {
+					getUsersInfo(id)
+				})
+			})
+		}
+		if (typetab === 'following') {
+			getFollowUser().then((res, req) => {
+				res.map((id) => {
+					getUsersInfo(id)
+				})
+			})
+		}
+
+	}, [typetab])
 	const menu = (
 		<Menu
 			items={[
@@ -44,28 +89,31 @@ export default function ProfileFollow(props) {
 	return (
 		<div className='profile-follow-content'>
 			<Row>
-				{data.map((ele, idx) => {
+				{usersInfo.map((ele, idx) => {
 					return (
 						<Col xs={24} sm={12} key={idx} justify="center">
-							<div className="container-item-friends">
-								<div>
-									<img alt="avata" src={ele.imgUrl} />
-									<span>{ele.name}</span>
+							<Link to={`profile/${ele._id}/newfeed`}>
+
+								<div className="container-item-friends">
+									<div>
+										<img alt="avata" src={ele.avatar} />
+										<span>{ele.fullName}</span>
+
+									</div>
+									{typetab === 'following' ?
+										<Dropdown
+											overlay={menu}
+											trigger={["click"]}
+											placement="bottomRight"
+										>
+											<BsThreeDotsVertical className="dropdown-btn" />
+										</Dropdown>
+										:
+										<></>
+									}
 
 								</div>
-								{typetab==='following' ?
-									<Dropdown
-										overlay={menu}
-										trigger={["click"]}
-										placement="bottomRight"
-									>
-										<BsThreeDotsVertical className="dropdown-btn" />
-									</Dropdown>
-									:
-									<></>
-								}
-
-							</div>
+							</Link>
 						</Col>
 					);
 				})}
