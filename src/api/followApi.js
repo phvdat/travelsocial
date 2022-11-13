1473abf26a118cfdc0f7f821ccc74fe685c39c1b
phvdat <phamvandat.hcmut@gmail.com>
2022-11-13 12:27:43 +0700

    add api getFollower and getFollowUser

--- src/api/followApi.js
+++ src/api/followApi.js
@@ -2,12 +2,20 @@ import axiosClient from "./axiosClient";
 
 const followApi = {
 	follow: (userIdTarget) => {
-		const url = '/follow-user';
-		return axiosClient.post(url, { userIdTarget });
+		const url = '/follow/follow-user';
+		return axiosClient.post(url, userIdTarget);
 	},
 	unFollow: (userIdTarget) => {
-		const url = '/follow-user';
-		return axiosClient.post(url, { userIdTarget });
+		const url = '/follow/follow-user';
+		return axiosClient.post(url, userIdTarget);
+	},
+	getFollower: (params) => {
+		const url = '/follow/get-follower';
+		return axiosClient.get(url, { params });
+	},
+	getFollowUser: (params) => {
+		const url = '/follow/get-follow-user';
+		return axiosClient.get(url, { params });
 	}
 }
 export default followApi;
