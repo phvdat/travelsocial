483a08a04325902a37030da8b0d16451decf68c6
phvdat <phamvandat.hcmut@gmail.com>
2022-11-21 02:42:29 +0700

    fix: crousel auto slide

--- src/components/slideshow/Slideshow.jsx
+++ src/components/slideshow/Slideshow.jsx
@@ -4,10 +4,10 @@ import React, { useEffect, useState } from "react";
 import "./slideshow.scss"
 export default function Slideshow() {
 	return (
-		<Carousel>
+		<Carousel autoplay>
 			<div className='carousel-container'>
 				<div className="text-carousel">ĐI VÀ KHÁM PHÁ THEO CÁCH CỦA BẠN</div>
-				<img alt="carousel" src="img/slideshow-3.jpg" className="imgSlideshow" />
+				<img alt="carousel" src="img/img_snow_wide.jpg" className="imgSlideshow" />
 			</div>
 			<div className='carousel-container'>
 				<img alt="carousel" src="img/slideshow-2.jpg" className="imgSlideshow" />
