cdf82a1d160d463a8b00df36f774735f91423487
phamvdat <dat.pham@lfglobaltech.com>
2022-12-05 10:34:09 +0700

    styles alert message

--- src/components/baseUI/alert/AlertMsg.jsx
+++ src/components/baseUI/alert/AlertMsg.jsx
@@ -1,14 +1,24 @@
+import moment from 'moment'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './alertMsg.scss'
 export default function AlertMsg(props) {
-	const { title, content, path } = props
+	const { notify } = props
+	const timeStamp = new Date(notify.createTime)
 	return (
-		<Link to={path}>
-			<div className={title ? "alert-msg" : ""}>
-				<h4>{title}</h4>
+		<Link to={notify.path}>
+			<div className="alert-msg">
+				<h4>Thông báo mới</h4>
 				<hr className='time-line' />
-				<p>{content}</p>
+				<div className='notify-item'>
+					<div className='avatar-content'>
+						<img src={notify.avatar} alt="avatar" />
+					</div>
+					<div className='text-content'>
+						<p>{notify.content}</p>
+						<span>{moment.utc(timeStamp.toUTCString()).fromNow()}</span>
+					</div>
+				</div>
 			</div>
 		</Link>
 
